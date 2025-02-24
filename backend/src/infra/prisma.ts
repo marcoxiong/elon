e9e898ccfadcec75ce359453b772fa7e2affe61d
Marco X <svetg212@gmail.com>
2025-02-23 20:58:39 -0800

    docker compose up

--- backend/src/infra/prisma.ts
+++ backend/src/infra/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from '@prisma/client';
+import ENV from '../configs/env';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: ENV.DATABASE_URL,
+    },
+  },
+});
 
 export default prisma;
